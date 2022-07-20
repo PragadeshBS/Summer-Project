07fe80dcb746c77247565ccc6afd1fdfd4ae8d23
Pragadesh BS <pragadeshbs@protonmail.com>
2022-07-20 11:42:07 +0530

    add participants

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -3,14 +3,29 @@ const {
   getEvents,
   createEvent,
   getEvent,
+  getParticipants,
+  updateEvent,
+  addParticipant,
 } = require("../controllers/eventController");
 
 const router = express.Router();
 
+// get all events
 router.get("/", getEvents);
 
+// get a specific event by id
 router.get("/:id", getEvent);
 
+// add a new event
 router.post("/", createEvent);
+
+// update a event
+router.patch("/:id", updateEvent);
+
+// add participants for an event
+router.post("/participants/:id", addParticipant);
+
+// get participants for an event
+router.get("/participants/:id", getParticipants);
 
 module.exports = router;
