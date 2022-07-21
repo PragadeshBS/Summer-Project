fa613d7feb892dc55da64f81a43b3eae830743ca
Pragadesh BS <pragadeshbs@protonmail.com>
2022-07-21 09:55:35 +0530

    backend for user creation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,14 @@
 const User = require("../models/userModel");
 
 const createUser = async (req, res) => {
-  const { userName, regNo, mobile, email } = req.body;
-  const user = await User.create({ userName, regNo, mobile, email });
+  const { userName, regNo, mobile, email, dept } = req.body;
+  const user = await User.create({ userName, regNo, mobile, email, dept });
   return res.json(user);
 };
 
 const getUserDetails = async (req, res) => {
-  const { username } = req.params;
-  const user = await User.findOne({ userName: username });
+  const { email } = req.params;
+  const user = await User.findOne({ email });
   if (!user) {
     return res.status(400).json({ error: "No such user" });
   }
