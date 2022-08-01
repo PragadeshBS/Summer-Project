4cbfa6f9fd79fa5e930e2a69c979ceb8321aba0b
Pragadesh BS <pragadeshbs@protonmail.com>
2022-08-01 12:35:19 +0530

    add datetime

--- frontend/src/pages/eventDetails/eventInformation.js
+++ frontend/src/pages/eventDetails/eventInformation.js
@@ -1,47 +1,56 @@
-const EventInformation = (props) => {
-    return <div>
-        <h1>Event Info</h1>
-        <div className="row ps-5">
-            <div className="col-lg-2">
-                <h5>Venue</h5>
-            </div>
-            <div className="col-lg-10">
-                <h6>{props.detail.venue}</h6>
-            </div>
-        </div>
-        <div className="row ps-5">
-            <div className="col-lg-2">
-                <h5>Contact Name</h5>
-            </div>
-            <div className="col-lg-10">
-                <h6>{props.detail.contactName}</h6>
-            </div>
-        </div>
-        <div className="row ps-5">
-            <div className="col-lg-2">
-                <h5>Contact Number</h5>
-            </div>
-            <div className="col-lg-10">
-                <h6>{props.detail.contactPhone}</h6>
-            </div>
-        </div>
-        <div className="row ps-5">
-            <div className="col-lg-2">
-                <h5>Email</h5>
-            </div>
-            <div className="col-lg-10">
-                <h6>{props.detail.contactEmail}</h6>
-            </div>
-        </div>
-        <div className="row ps-5">
-            <div className="col-lg-2">
-                <h5>Description</h5>
-            </div>
-            <div className="col-lg-10">
-                <h6>{props.detail.otherInfo}</h6>
-            </div>
-        </div>
-    </div>
-}
+import {format} from "date-fns"
 
-export default EventInformation
+const EventInformation = (props) => {
+  return (
+    <div>
+      <h1>Event Info</h1>
+      <h1 className="lead my-3">
+        {format(new Date(props.detail.eventStartDate), "dd MMM yyyy\th:mm a")}
+        {"\t"}-{"\t"}
+        {format(new Date(props.detail.eventEndDate), "dd MMM yyyy\th:mm a")}
+      </h1>
+      <div className="row ps-5">
+        <div className="col-lg-2">
+          <h5>Venue</h5>
+        </div>
+        <div className="col-lg-10">
+          <h6>{props.detail.venue}</h6>
+        </div>
+      </div>
+      <div className="row ps-5">
+        <div className="col-lg-2">
+          <h5>Contact Name</h5>
+        </div>
+        <div className="col-lg-10">
+          <h6>{props.detail.contactName}</h6>
+        </div>
+      </div>
+      <div className="row ps-5">
+        <div className="col-lg-2">
+          <h5>Contact Number</h5>
+        </div>
+        <div className="col-lg-10">
+          <h6>{props.detail.contactPhone}</h6>
+        </div>
+      </div>
+      <div className="row ps-5">
+        <div className="col-lg-2">
+          <h5>Email</h5>
+        </div>
+        <div className="col-lg-10">
+          <h6>{props.detail.contactEmail}</h6>
+        </div>
+      </div>
+      <div className="row ps-5">
+        <div className="col-lg-2">
+          <h5>Description</h5>
+        </div>
+        <div className="col-lg-10">
+          <h6>{props.detail.otherInfo}</h6>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EventInformation;
